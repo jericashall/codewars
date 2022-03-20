6b0d15e5e948029c9406f507095d58c271f4025e
Jerica Hall <jericashall@gmail.com>
2022-03-20 14:01:09 -0400

    March 20th codewars

--- DNA to RNA conversion.js
+++ DNA to RNA conversion.js
@@ -1,4 +1,4 @@
-// March 14th - 8 kyu
+// March 14th - 8 kyu   
 /* Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
 Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
--- Even or Odd.js
+++ Even or Odd.js
@@ -1,4 +1,4 @@
-// March 17th - 8 kyu
+// March 17th - 8 kyu    
 /* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
 
 function even_or_odd(number) {
