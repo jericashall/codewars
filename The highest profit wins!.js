34b1f991e5f20fc967db5b6f419250c851abe385
Jerica Hall <jericashall@gmail.com>
2022-03-21 14:29:26 -0400

    March 21st code wars

---
+++ The highest profit wins!.js
@@ -0,0 +1,25 @@
+// March 21st - 7 kyu
+
+/*Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.*/
+
+function minMax(arr){
+    let min = Infinity;
+    let max = -Infinity;
+    arr.forEach((num,i) => {
+      if (num < min) {
+        min = num;
+      }
+      if (num > max) {
+        max = num;
+      }
+    })
+    let minAndMax = [min, max];
+    return minAndMax;
+  }
