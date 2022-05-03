83426b1b44dbf17cfa42f023bd4bf490b12be696
Jerica Hall <jericashall@gmail.com>
2022-05-02 21:53:00 -0400

    May 2nd daily codewars

---
+++ Digits explosion.js
@@ -0,0 +1,16 @@
+//May 2nd - 7 kyu
+/*Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+explode("312")
+should return :
+
+"333122"
+explode("102269")
+should return :
+
+"12222666666999999999"*/
+
+function explode(s) {
+    return s.split('').map(x => x.repeat(+x)).join('')
+  }
