f47c78c076aba941e2e74e847389e8c156fc9db8
jericashall <jericashall@gmail.com>
2022-07-01 20:59:06 -0700

    July 1st codewars

---
+++ Testing 1-2-3.js
@@ -0,0 +1,23 @@
+/*Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]*/
+
+var number=function(array){
+    return array.map((x,i) => `${i+1}: ${x}`)
+  }
+
+/*tests*/
+
+describe("Tests", () => {
+    it("test", () => {
+  Test.assertSimilar(number([]), [], 'Empty array should return empty array');
+  Test.assertSimilar(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');                
+    });
+  });
