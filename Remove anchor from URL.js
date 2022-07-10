43e116f1ec0007e6e0378b50f8753e6020e68b9b
Jerica Hall <jericashall@gmail.com>
2022-07-09 23:46:41 -0400

    July 9th codewars

---
+++ Remove anchor from URL.js
@@ -0,0 +1,10 @@
+/*Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"*/
+
+function removeUrlAnchor(url){
+    let arr = url.split("#")
+    return arr[0]
+  }
