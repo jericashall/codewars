941ffd0c9261b6a557692d4ba9afb2b8c759709d
Jerica Hall <jericashall@gmail.com>
2022-07-12 10:05:14 -0400

    July 12th codewars

---
+++ palindrome.js
@@ -0,0 +1,26 @@
+/*Write a function that checks if a given string (case insensitive) is a palindrome.*/
+
+function isPalindrome(x) {
+    let word = x .toLowerCase()
+    for(let i = 0; i < word.length; i++) {
+      if(word[i] !== word[word.length-1-i]) {
+        return false
+      }
+    }
+    return true
+  }
+
+/*tests*/
+
+const { assert } = require('chai');
+
+describe("Fixed tests", function() {
+  it("Testing for 'a'", () => assert.strictEqual(isPalindrome("a"), true));
+  it("Testing for 'aba'", () => assert.strictEqual(isPalindrome("aba"), true));
+  it("Testing for 'Abba'", () => assert.strictEqual(isPalindrome("Abba"), true));
+  it("Testing for 'hello'", () => assert.strictEqual(isPalindrome("hello"), false));
+  it("Testing for 'Bob'", () => assert.strictEqual(isPalindrome("Bob"), true));
+  it("Testing for 'Madam'", () => assert.strictEqual(isPalindrome("Madam"), true));
+  it("Testing for 'AbBa'", () => assert.strictEqual(isPalindrome("AbBa"), true));
+  it("Testing for ''", () => assert.strictEqual(isPalindrome(""), true));
+});
