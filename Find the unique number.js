f8d52b9660b47b1c7bf337d85274b3ee2226c2ab
Jerica Hall <jericashall@gmail.com>
2022-09-15 03:56:21 -0400

    codewars

---
+++ Find the unique number.js
@@ -0,0 +1,40 @@
+/*There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.*/
+
+function findUniq(arr) {
+    let set = new Set();
+    set.add(arr[0]);
+    let i = 0;
+    while(set.has(arr[i])) {
+      i++
+    }
+    if (i === 1) {
+      if(arr[1] === arr[2]) {
+        return arr[0]
+      }
+    }
+    return arr[i]
+  }
+
+  /*tests*/
+
+  const chai = require("chai");
+  const assert = chai.assert;
+  chai.config.truncateThreshold = 0;
+  
+  describe("Example Tests", () => {
+    it("Tests", () => {
+      assert.strictEqual(findUniq([ 1, 0, 0 ]), 1);
+      assert.strictEqual(findUniq([ 0, 1, 0 ]), 1);
+      assert.strictEqual(findUniq([ 0, 0, 1 ]), 1);
+      assert.strictEqual(findUniq([ 1, 1, 1, 2, 1, 1 ]), 2);
+      assert.strictEqual(findUniq([ 1, 1, 2, 1, 1 ]), 2);
+      assert.strictEqual(findUniq([ 3, 10, 3, 3, 3 ]), 10);
+    });
+  });
+  
