537e3388e0cae77525e0d36df2cf791b8cbf2d51
Jerica Hall <jericashall@gmail.com>
2022-10-21 22:41:22 -0400

    codewars

---
+++ return the day.js
@@ -0,0 +1,16 @@
+/*Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"*/
+
+function whatday(num) {
+    if(num > 7 || num === 0) return "Wrong, please enter a number between 1 and 7"
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return weekdays[num-1]
+  }
